262aeeb0c6f9740f349d3752d06429e705237729
Jack Greiner <jack@emoss.org>
2024-10-09 11:49:39 -0400

    local-build.sh: Quote variables to prevent potential problems

--- local-build.sh
+++ local-build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-BRANCH=test
+BRANCH="test"
 
 rm -f tech.feliciano.pocket-casts.flatpak
 rm -rf _build ; mkdir _build
 rm -rf _repo ; mkdir _repo
 
-flatpak-builder --ccache --force-clean --default-branch=$BRANCH _build tech.feliciano.pocket-casts.yml --repo=_repo
-flatpak build-bundle _repo tech.feliciano.pocket-casts.flatpak tech.feliciano.pocket-casts $BRANCH
+flatpak-builder --ccache --force-clean --default-branch="$BRANCH" _build tech.feliciano.pocket-casts.yml --repo=_repo
+flatpak build-bundle _repo tech.feliciano.pocket-casts.flatpak tech.feliciano.pocket-casts "$BRANCH"
